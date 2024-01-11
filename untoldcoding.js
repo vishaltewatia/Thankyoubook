b7080ff8ed54b87e9b4e44c1d9d3806df5da996b
Untold Coding <113816767+vishaltewatia@users.noreply.github.com>
2024-01-11 18:43:25 +0530

    Add files via upload

---
+++ untoldcoding.js
@@ -0,0 +1,21 @@
+let currentPage = 1;
+
+function toggleClass(e, toggleClassName) {
+  if (e.className.includes(toggleClassName)) {
+    e.className = e.className.replace(" " + toggleClassName, "");
+  } else {
+    e.className += " " + toggleClassName;
+  }
+}
+
+function movePage(e, page) {
+  if (page == currentPage) {
+    currentPage += 2;
+    toggleClass(e, "left-side");
+    toggleClass(e.nextElementSibling, "left-side");
+  } else if ((page = currentPage - 1)) {
+    currentPage -= 2;
+    toggleClass(e, "left-side");
+    toggleClass(e.previousElementSibling, "left-side");
+  }
+}
